cab7be1a99c3f3c849af1dbd10e9b9e4305cb819
AYKdgreat <dalemoayokunlephilip@gmail.com>
2022-04-23 02:53:01 +0100

    midnight

--- create.php
+++ create.php
@@ -1,14 +1,23 @@
 <?php 
-
+$title = $tags = $body = '';
+$error = ['msg' => ''];
 
 if(isset($_POST['create-post'])){
-  $title = $_POST['post-title'];
-  $tags = $_POST['post-tags'];
+  $title = htmlspecialchars($_POST['post-title']);
+  $tags = htmlspecialchars($_POST['post-tags']);
   $body = $_POST['post-body'];
   if(!empty($title) && !empty($tags) &&!empty($body)){
-    echo "okay";
+    if(!(str_word_count($title) > 30 || str_word_count($body) > 750)) {
+      echo $body.'<br>';
+      echo $title;
+      if (!preg_match('/^[a-zA-Z\'()!,.\s]+$/', $title)) {
+        $error['msg'] = "Invalid title, special characters & numbers not allowed";
+      }
+    } else {
+      $error['msg'] = 'Title = 30 words max, Body = 750 words max!';
+    }
   } else {
-    echo "not okay";
+    $error['msg'] = 'Fill all required fields *';
   }
 }
 
@@ -22,19 +31,23 @@ include "inc/header.php";
 ?>
 
   <div class="card">
-    <form action="" class="create-form" method="post">
+    <form action="" class="create-form" method="post" enctype=multipart/form-data>
     <h1 class="create-heading">Create New Post</h1>
       <div class="input-group">
-        <label for="title">Post Title</label>
-        <input type="text" class="input-field" name="post-title">
+        <label for="title"><span>Post Thumbnail</span><span class="error"><?php echo $error['msg']?></span></label>
+        <input type="file" class="input-field" name="post-title" value="<?php echo $title; ?>">
       </div>
       <div class="input-group">
-        <label for="title">Post Tags</label>
-        <input type="text" class="input-field" name="post-tags">
+        <label for="title"><span>Post Title *</span> <span id="title_word_count"><b>0/30</b></span></label>
+        <input type="text" class="input-field" id="post-title" name="post-title" onkeyup="titleWordCount()" value="<?php echo $title; ?>">
       </div>
       <div class="input-group">
-        <label for="title">Post Body</label>
-        <textarea class="input-field textarea" name="post-body"></textarea>
+        <label for="post-tags"><span> Post Tags *</span></label>
+        <input type="text" class="input-field" name="post-tags" value="<?php echo $tags; ?>">
+      </div>
+      <div class="input-group">
+        <label for="post-body"><span> Post Body *</span><span id="body_word_count"><b>0/750</b></span></label>
+        <textarea class="input-field textarea" id="post-body" name="post-body" onkeyup="bodyWordCount()"><?php echo $body; ?></textarea>
       </div>
       <input type="submit" name="create-post" value="Create post" class="create-btn" />
     </form>
@@ -86,4 +99,39 @@ include "inc/header.php";
         </div>
       </section>
     </aside>
+    <script>
+    function titleWordCount(){
+      let t_eval_ = document.getElementById('post-title')
+      let title_word_count = document.getElementById('title_word_count')
+      t_eval__ = t_eval_.value.split(' ')
+      t_eval___ = t_eval__.filter(function(str) {
+        return str != ""
+      })
+      title_word_count.innerHTML = t_eval___.length + "/30"
+      if(t_eval___.length >= 20) {
+        title_word_count.style.color = '#f00'
+        title_word_count.style.fontWeight = 'bold'
+      } else {
+        title_word_count.style.color = '#000'
+        title_word_count.style.fontWeight = 'bold'
+      }
+    }
+
+    function bodyWordCount(){
+      let b_eval_ = document.getElementById('post-body')
+      let body_word_count = document.getElementById('body_word_count')
+      b_eval__ = b_eval_.value.split(' ')
+      b_eval___ = b_eval__.filter(function(str) {
+        return str != ""
+      })
+      body_word_count.innerHTML = b_eval___.length + "/750"
+      if(b_eval___.length >= 700) {
+        body_word_count.style.color = '#f00'
+        body_word_count.style.fontWeight = 'bold'
+      } else {
+        body_word_count.style.color = '#000'
+        body_word_count.style.fontWeight = 'bold'
+      }
+    }
+    </script>
   <?php include "inc/footer.php"; ?>
---
+++ klblog.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2022 at 01:50 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `klblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `username` varchar(11) NOT NULL,
+  `comment-body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `body` text NOT NULL,
+  `tags` varchar(100) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
